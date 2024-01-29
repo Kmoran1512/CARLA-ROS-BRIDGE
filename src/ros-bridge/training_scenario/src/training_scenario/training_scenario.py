c6e1a7b1f45f07b3458ee9012b77398ff6523ad9
Kmoran1512 <kmoran.1512@gmail.com>
2024-01-29 09:20:37 -0500

    SpawnObject n pedestrians

--- src/ros-bridge/training_scenario/src/training_scenario/training_scenario.py
+++ src/ros-bridge/training_scenario/src/training_scenario/training_scenario.py
@@ -1,129 +1,72 @@
-import carla
-import logging
 import rclpy
-import random
-import time
 
-from carla_ros_bridge.actor_factory import ActorFactory
 from rclpy.node import Node
 
+from carla_msgs.srv import SpawnObject, DestroyObject
 
 class TrainingScenario(Node):
-    def __init__(self, client):
+    def __init__(self):
         super(TrainingScenario, self).__init__("TrainingScenario")
 
         self.declare_parameter('role_name', 'hero')
-
-        self.client = client
         role_name = self.get_parameter('role_name')
 
+        self.spawn_actors_service = self.create_client(SpawnObject, '/carla/spawn_object')
+        self.destroy_object_service = self.create_client(DestroyObject, "/carla/destroy_object")
+
         self.walkers = []
-        self.w_id = []
 
-        print(f"in {role_name.value}")
 
-    def spawn_walkers(self, client, world, n=10):
-        world = client.get_world()
-        bps = world.get_blueprint_library().filter("walker.pedestrian.*")
+    def spawn_walkers(self, n=1):
+        walker_requests = []
 
-        walkers_list = []
-        all_id = []
-
-        percentagePedestriansRunning = 0.0  # how many pedestrians will run
-        percentagePedestriansCrossing = (
-            0.9  # how many pedestrians will walk through the road
-        )
-
-        world.set_pedestrians_cross_factor(percentagePedestriansCrossing)
-
-        spawn_points = []
         for i in range(n):
-            spawn_point = carla.Transform()
-            loc = world.get_random_location_from_navigation()
-            if loc != None:
-                spawn_point.location = loc
-                spawn_points.append(spawn_point)
+            walker_request = SpawnObject.Request()
 
-        batch = []
-        walker_speed = []
+            number = i % 44
 
-        for spawn_point in spawn_points:
-            bp = random.choice(bps)
+            walker_request.type = f'walker.pedestrian.00{number:02}'
+            walker_request.id = f'walker{i:02}'
+            walker_request.random_pose = True
 
-            bp.set_attribute("is_invincible", "false")
+            walker_requests.append(self.spawn_actors_service.call_async(walker_request))
 
-            # Adjust for running here
-            walker_speed.append(bp.get_attribute("speed").recommended_values[1])
+        for request in walker_requests:
+            future = request
+            rclpy.spin_until_future_complete(self, future)
+            self.walkers.append(future.result().id)
+        
+        self.walkers = [walker for walker in self.walkers if walker > 0]
+        self.get_logger().info(f"spawned {len(self.walkers)} walkers")
 
-            batch.append(carla.command.SpawnActor(bp, spawn_point))
+    def clean_up(self):
+        responses = []
 
-        results = client.apply_batch_sync(batch, True)
+        for id in self.walkers:
+            remove_req = DestroyObject.Request()
+            remove_req.id = id
+            responses.append(self.destroy_object_service.call_async(remove_req))
 
-        walker_speed2 = []
-        for i in range(len(results)):
-            if results[i].error:
-                self.logger.error(results[i].error)
-            else:
-                walkers_list.append({"id": results[i].actor_id})
-                walker_speed2.append(walker_speed[i])
-        walker_speed = walker_speed2
 
-        batch = []
-        walker_controller_bp = world.get_blueprint_library().find("controller.ai.walker")
-        for i in range(len(walkers_list)):
-            batch.append(
-                carla.command.SpawnActor(
-                    walker_controller_bp, carla.Transform(), walkers_list[i]["id"]
-                )
-            )
-        results = client.apply_batch_sync(batch, True)
-        for i in range(len(results)):
-            if results[i].error:
-                self.logger.error(results[i].error)
-            else:
-                walkers_list[i]["controller"] = results[i].actor_id
+        for response in responses:
+            rclpy.spin_until_future_complete(self, response)
 
-        for walker in walkers_list:
-            all_id.append(walker["controller"])
-            all_id.append(walker["id"])
-        all_actors = world.get_actors(all_id)
-
-        world.tick()
-
-        for i in range(0, len(all_id), 2):
-            all_actors[i].start()
-            all_actors[i].go_to_location(world.get_random_location_from_navigation())
-            all_actors[i].set_max_speed(float(walker_speed[int(i / 2)]))
-
-        self.logger.info("spawned %d walkers, press Ctrl+C to exit." % (len(walkers_list)))
-
-        self.walkers = all_actors
-        self.w_id = all_id
-
-    def clean_up(self, client):
-        for i in range(0, len(self.w_id), 2):
-            self.walkers[i].stop()
-
-        print("\ndestroying %d walkers" % len(self.walkers))
-        client.apply_batch([carla.command.DestroyActor(x) for x in self.w_id])
-
+        self.get_logger().info(f"removed {len(responses)} walkers")
 
 
 def main():
-    carla_client = carla.Client('localhost', 2000)
-    world = carla_client.get_world()
 
     rclpy.init()
 
-    ts = TrainingScenario(world)
-    ts.spawn_walkers(carla_client, world, 200)
+    ts = TrainingScenario()
+    ts.spawn_walkers(1)
 
     try:
         rclpy.spin(ts)
     except KeyboardInterrupt:
         pass
 
-    ts.clean_up(carla_client)
+    ts.clean_up()
     ts.destroy_node()
     rclpy.shutdown()
 
