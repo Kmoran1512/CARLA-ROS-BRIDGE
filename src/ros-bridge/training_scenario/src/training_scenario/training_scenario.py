e853fb38e9b4109ed523d6f06d5ac54800cbde3f
Kmoran1512 <kmoran.1512@gmail.com>
2024-01-29 09:40:20 -0500

    stub out walker agent

--- src/ros-bridge/training_scenario/src/training_scenario/training_scenario.py
+++ src/ros-bridge/training_scenario/src/training_scenario/training_scenario.py
@@ -1,21 +1,28 @@
+import math
 import rclpy
 
 from rclpy.node import Node
 
 from carla_msgs.srv import SpawnObject, DestroyObject
+from carla_msgs.msg import CarlaWalkerControl
+from geometry_msgs.msg import Pose, Vector3
+
 
 class TrainingScenario(Node):
     def __init__(self):
         super(TrainingScenario, self).__init__("TrainingScenario")
 
-        self.declare_parameter('role_name', 'hero')
-        role_name = self.get_parameter('role_name')
+        self.declare_parameter("role_name", "hero")
+        role_name = self.get_parameter("role_name")
 
-        self.spawn_actors_service = self.create_client(SpawnObject, '/carla/spawn_object')
-        self.destroy_object_service = self.create_client(DestroyObject, "/carla/destroy_object")
+        self.spawn_actors_service = self.create_client(
+            SpawnObject, "/carla/spawn_object"
+        )
+        self.destroy_object_service = self.create_client(
+            DestroyObject, "/carla/destroy_object"
+        )
 
         self.walkers = []
-
 
     def spawn_walkers(self, n=1):
         walker_requests = []
@@ -25,8 +32,8 @@ class TrainingScenario(Node):
 
             number = i % 44
 
-            walker_request.type = f'walker.pedestrian.00{number:02}'
-            walker_request.id = f'walker{i:02}'
+            walker_request.type = f"walker.pedestrian.00{number:02}"
+            walker_request.id = f"walker{i:02}"
             walker_request.random_pose = True
 
             walker_requests.append(self.spawn_actors_service.call_async(walker_request))
@@ -35,7 +42,7 @@ class TrainingScenario(Node):
             future = request
             rclpy.spin_until_future_complete(self, future)
             self.walkers.append(future.result().id)
-        
+
         self.walkers = [walker for walker in self.walkers if walker > 0]
         self.get_logger().info(f"spawned {len(self.walkers)} walkers")
 
@@ -47,15 +54,66 @@ class TrainingScenario(Node):
             remove_req.id = id
             responses.append(self.destroy_object_service.call_async(remove_req))
 
-
         for response in responses:
             rclpy.spin_until_future_complete(self, response)
 
         self.get_logger().info(f"removed {len(responses)} walkers")
 
 
-def main():
+class WalkerAgent(Node):
+    MIN_DISTANCE = 0.5
 
+    def __init__(self, number, id):
+        self.id = id
+        self.n = number
+
+        self._waypoints = []
+        self._pose = Vector3()
+
+        self.walker_publisher = self.new_publisher(
+            CarlaWalkerControl,
+            "/carla/{}/walker_control_cmd".format(f"walker{self.n:02}"),
+            10,
+        )
+
+    def run_step(self):
+        # TODO: stay on sidewalk
+        if not self._waypoints:
+            return
+
+        control = CarlaWalkerControl()
+        direction = Vector3()
+        direction.x = self._waypoints[-1].position.x - self._pose.position.x
+        direction.y = self._waypoints[-1].position.y - self._pose.position.y
+        direction_norm = math.sqrt(direction.x**2 + direction.y**2)
+        if direction_norm > WalkerAgent.MIN_DISTANCE:
+            control.speed = self._target_speed
+            control.direction.x = direction.x / direction_norm
+            control.direction.y = direction.y / direction_norm
+        else:
+            self._waypoints.pop()
+            if self._waypoints:
+                self.loginfo(
+                    "next waypoint: {} {}".format(
+                        self._waypoints[-1].position.x, self._waypoints[-1].position.y
+                    )
+                )
+            else:
+                self._reached_goal()
+
+        self.control_publisher.publish(control)
+
+    def get_position(self, objects_list):
+        self._pose.x = 0
+        self._pose.y = 0
+        pass
+
+    def _reached_goal(self):
+        self.get_logger().info("Route finished.")
+        pass
+
+
+def main():
     rclpy.init()
 
     ts = TrainingScenario()
@@ -71,5 +129,5 @@ def main():
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
