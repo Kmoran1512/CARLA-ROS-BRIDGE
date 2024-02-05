d55f35aecca7060a057a37027e920b1d0d8150fd
Kmoran1512 <kmoran.1512@gmail.com>
2024-02-05 11:36:58 -0500

    Wlakers walking

--- src/ros-bridge/training_scenario/src/training_scenario/training_scenario.py
+++ src/ros-bridge/training_scenario/src/training_scenario/training_scenario.py
@@ -4,12 +4,10 @@ import rclpy
 
 from rclpy.node import Node
 
+import carla_common.transforms as trans
 from carla_msgs.srv import SpawnObject, DestroyObject
 from carla_msgs.msg import CarlaTrafficLightStatusList
-from derived_object_msgs.msg import ObjectArray
-
-
-SYNC = False
+from geometry_msgs.msg import Pose
 
 
 class TrainingScenario(Node):
@@ -23,7 +21,11 @@ class TrainingScenario(Node):
         self.client.set_timeout(5.0)
         self.world = self.client.get_world()
 
+        self.world.set_pedestrians_cross_factor(0.7)
+
         self.walkers = {}
+
+        self.walker_agents = {}
         self.spawn_actors_service = self.create_client(
             SpawnObject, "/carla/spawn_object"
         )
@@ -36,28 +38,32 @@ class TrainingScenario(Node):
         )
 
     def clean_up(self):
-        # Perhaps Save this for walker agents
-        pass
+        self.client.apply_batch(
+            [carla.command.DestroyActor(x) for x in self.walker_agents.keys()]
+        )
+
+    def run_step(self):
+        if not len(self.walker_agents) > 0:
+            return
+
+        self.world.wait_for_tick()
 
     def spawn_walkers(self):
         if not self.spawn_actors_service.wait_for_service(timeout_sec=10.0):
-            self.get_logger().error('Service spawn_actors not available after waiting')
+            self.get_logger().error("Service spawn_actors not available after waiting")
             return
 
-
+        spawn_points = self._get_spawn_location()
         walker_requests = []
 
-        pedestrians_left_to_spawn = self.pedestrian_number + 1 - len(self.walkers)
-
-        for i in range(1, pedestrians_left_to_spawn):
+        for i in range(self.pedestrian_number):
             walker_request = SpawnObject.Request()
 
-            number = i % 44
+            number = (i % 44) + 1
 
             walker_request.type = f"walker.pedestrian.{number:04}"
             walker_request.id = f"walker{i:04}"
-            # TODO: Set Pose Here
-            walker_request.random_pose = True
+            walker_request.transform = spawn_points[i]
 
             walker_requests.append(self.spawn_actors_service.call_async(walker_request))
 
@@ -69,6 +75,41 @@ class TrainingScenario(Node):
                 self.walkers[walker_id] = self.world.get_actor(walker_id)
 
         self.get_logger().info(f"\n\n\n spawned {len(self.walkers)} walkers \n\n\n")
+        self._walk()
+
+    def _walk(self):
+        walker_controller_bp = self.world.get_blueprint_library().find(
+            "controller.ai.walker"
+        )
+
+        batch = []
+
+        self.get_logger().info(f"length_walkers  ::  {len(self.walkers)}")
+        for walker_id in self.walkers.keys():
+            batch.append(
+                carla.command.SpawnActor(
+                    walker_controller_bp, carla.Transform(), walker_id
+                )
+            )
+
+        results = self.client.apply_batch_sync(batch, True)
+
+        for result in results:
+            if result.error:
+                self.get_logger().error(result.error)
+            else:
+                self.walker_agents[result.actor_id] = self.world.get_actor(
+                    result.actor_id
+                )
+
+        for _, agent in self.walker_agents.items():
+            agent.start()
+            agent.go_to_location(self.world.get_random_location_from_navigation())
+            agent.set_max_speed(2.0)
+
+        self.get_logger().info(
+            f"\n\n\n Agents spawned {len(self.walker_agents)} \n\n\n"
+        )
 
     def traffic_light_status(self, traffic_light_info_msg):
         lights_on = []
@@ -81,6 +122,19 @@ class TrainingScenario(Node):
             return
         all_actors = self.world.get_actors(lights_on)
         self._turn_off_lights(all_actors)
+
+    def _get_spawn_location(self):
+        spawn_poses = []
+
+        for _ in range(self.pedestrian_number):
+            spawn_pose = Pose()
+
+            spawn_point = self.world.get_random_location_from_navigation()
+            spawn_pose.position = trans.carla_location_to_ros_point(spawn_point)
+
+            spawn_poses.append(spawn_pose)
+
+        return spawn_poses
 
     def _turn_off_lights(self, light_actors):
         for actor in light_actors:
@@ -97,10 +151,13 @@ def main():
     ts.spawn_walkers()
 
     try:
-        rclpy.spin(ts)
+        ts.create_timer(0.05, lambda timer_event=None: ts.run_step())
+
+        rclpy.spin(ts, ts.executor)
     except KeyboardInterrupt:
         pass
 
+    ts.clean_up()
     ts.destroy_node()
     rclpy.shutdown()
 
@@ -109,9 +166,10 @@ if __name__ == "__main__":
     main()
 
 # TODO:
-# - Get spawn points from the blueprints
+# - ~~Get spawn points from the blueprints~~
 # - ~~SpawnActor service to create actors~~
 # - ~~Wait for the response~~
+# - ~~Ensure the car doesn't **stop** for walkers on the sidewalk~~
 # - Attach a walker agent to the actor
 # - Give the actor a goal position
 # - Walk them around
