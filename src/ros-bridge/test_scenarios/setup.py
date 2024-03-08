a3e00c1b4d403a3a978bf2107c532c6c599264af
Kmoran1512 <kmoran.1512@gmail.com>
2024-03-08 11:35:30 -0500

    using new second json

--- src/ros-bridge/test_scenarios/setup.py
+++ src/ros-bridge/test_scenarios/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name + "/scenarios", ["scenarios/crossing.json"]),
+        ("share/" + package_name + "/scenarios", ["scenarios/crossing.json", "scenarios/crossing_copy.json"]),
         (os.path.join("share", package_name), glob("launch/*.launch.py")),
     ],
     install_requires=["setuptools"],
