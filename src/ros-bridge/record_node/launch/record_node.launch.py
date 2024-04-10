0b689df2e17a3abe59c3c490fd2800efa7378675
Kmoran1512 <kmoran.1512@gmail.com>
2024-04-10 13:41:30 -0400

    record bag

--- src/ros-bridge/record_node/launch/record_node.launch.py
+++ src/ros-bridge/record_node/launch/record_node.launch.py
@@ -1,9 +1,8 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
@@ -11,52 +10,78 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    ld = LaunchDescription(
-        [
-            DeclareLaunchArgument(name="record_gaze", default_value="False"),
-            DeclareLaunchArgument(name="participant_number", default_value="1"),
-            DeclareLaunchArgument(name="scenario_number", default_value="1"),
-            DeclareLaunchArgument(name="draw_manctrl", default_value="True"),
-            DeclareLaunchArgument(name="draw_gaze", default_value="False"),
-            DeclareLaunchArgument(name="draw_outline", default_value="False"),
-            DeclareLaunchArgument(name="draw_route", default_value="False"),
-            DeclareLaunchArgument(name="labels", default_value="[]]"),
-            # Run Node
-            Node(
-                package="record_node",
-                namespace="img_publisher1",
-                executable="img_publisher",
-                output="screen",
-                name="test",
-                parameters=[
-                    {"draw_manctrl": LaunchConfiguration("draw_manctrl")},
-                    {"draw_gaze": LaunchConfiguration("draw_gaze")},
-                    {"draw_outline": LaunchConfiguration("draw_outline")},
-                    {"draw_route": LaunchConfiguration("draw_route")},
-                    {"labels": LaunchConfiguration("labels")},
-                ],
-            ),
-            Node(
-                package="record_node",
-                namespace="record_node1",
-                executable="record_node",
-                output="screen",
-                name="test",
-                parameters=[
-                    {"record_gaze": LaunchConfiguration("record_gaze")},
-                    {"participant_number": LaunchConfiguration("participant_number")},
-                    {"scenario_number": LaunchConfiguration("scenario_number")},
-                ],
-            ),
-            Node(
-                package="record_node",
-                namespace="semantic_boxes1",
-                executable="semantic_boxes",
-                output="screen",
-                name="test",
-            ),
-        ]
+    launch_args = [
+        DeclareLaunchArgument(name="record_gaze", default_value="False"),
+        DeclareLaunchArgument(name="participant_number", default_value="1"),
+        DeclareLaunchArgument(name="scenario_number", default_value="1"),
+        DeclareLaunchArgument(name="draw_manctrl", default_value="True"),
+        DeclareLaunchArgument(name="draw_gaze", default_value="False"),
+        DeclareLaunchArgument(name="draw_outline", default_value="False"),
+        DeclareLaunchArgument(name="draw_route", default_value="False"),
+        DeclareLaunchArgument(name="labels", default_value="[]]"),
+        # Run Node
+        Node(
+            package="record_node",
+            namespace="img_publisher1",
+            executable="img_publisher",
+            output="screen",
+            name="test",
+            parameters=[
+                {"draw_manctrl": LaunchConfiguration("draw_manctrl")},
+                {"draw_gaze": LaunchConfiguration("draw_gaze")},
+                {"draw_outline": LaunchConfiguration("draw_outline")},
+                {"draw_route": LaunchConfiguration("draw_route")},
+                {"labels": LaunchConfiguration("labels")},
+            ],
+        ),
+        Node(
+            package="record_node",
+            namespace="record_node1",
+            executable="record_node",
+            output="screen",
+            name="test",
+            parameters=[
+                {"record_gaze": LaunchConfiguration("record_gaze")},
+                {"participant_number": LaunchConfiguration("participant_number")},
+                {"scenario_number": LaunchConfiguration("scenario_number")},
+            ],
+        ),
+        Node(
+            package="record_node",
+            namespace="semantic_boxes1",
+            executable="semantic_boxes",
+            output="screen",
+            name="test",
+        ),
+        Node(
+            package="record_node", executable="bag_remap", output="screen", name="test"
+        ),
+    ]
+
+    launch_args.append(
+        ExecuteProcess(
+            cmd=[
+                "ros2",
+                "bag",
+                "record",
+                "/resized_semantic",
+                "/resized_rgb",
+                "--qos-profile-overrides-path",
+                os.path.join(
+                    get_package_share_directory("record_node"),
+                    "config",
+                    "bag_config.yaml",
+                ),
+                "--compression-mode",
+                "file",
+                "--compression-format",
+                "zstd",
+            ]
+        )
     )
+
+    ld = LaunchDescription(launch_args)
+
     return ld
 
 
